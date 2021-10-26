2810dd1084d5b79a098568699f289d611c5497bb
bojanabb <bojana.buljic@gmail.com>
2021-10-26 13:22:40 +0200

    Add Header and Navbar component

---
+++ src/Components/Header.js
@@ -0,0 +1,10 @@
+import Navbar from "./Navbar";
+import './Header.css';
+
+export default function Header() {
+    return (
+        <header className="header__container">
+            <Navbar />
+        </header>
+    );
+}
