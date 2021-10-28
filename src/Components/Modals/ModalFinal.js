c9257e25123d521d1bb663e4266561ade48f2dc6
bojanabb <bojana.buljic@gmail.com>
2021-10-28 10:50:24 +0200

    Add ModalFinal

--- src/Components/Modals/ModalFinal.js
+++ src/Components/Modals/ModalFinal.js
@@ -1,1 +1,17 @@
 import '../Modals/Modals.css';
+import { ReactComponent as IconChecked } from './../../assets/icon-check.svg';
+import { useMyContext } from '../../context/Context';
+
+export default function ModalFinal() {
+    const isModalVisible = useMyContext()[4],
+        setModalVisible = useMyContext()[5];
+
+    return (
+        <div className={isModalVisible ? "modal-completed visible" : "modal-completed"}>
+            <div className="icon-checked"><IconChecked alt="checked-icon" onClick={() => setModalVisible(false)} /></div>
+            <h1 className="modal-completed-title">Thanks for your support!</h1>
+            <p className="modal-completed-text text">Your pledge brings us one step closer to sharing Mastercraft Bamboo Monitor Riser worldwide. You will get an email once our campaign is completed.</p>
+            <button className="modal-completed-btn js-got-it" onClick={() => setModalVisible(false)}>Got it!</button>
+        </div>
+    )
+}
